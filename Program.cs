2d0433dd0c6fe3eac7911a7b157427789e3b085a
marius.herciu23 <marius.herciu23@gmail.com>
2022-05-09 10:20:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace laborator3Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se citesc doua numere de la tastatura. Scrieti un program care va
+            //calcula cel mai mare divizor comun al numerelor
+
+            Console.WriteLine("Introduceti 2 numere:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int divizor = MaxDivizorComun(a, b);
+            Console.WriteLine("Cel mai mare divizor comun este " + divizor);
+        }
+
+        static int MaxDivizorComun(int a, int b)
+        {
+            int divizor;
+
+            if (a == b)
+            {
+                divizor = a;
+                return divizor;
+            }
+
+            divizor = Math.Min(a, b);
+            for (int i = 0; i < Math.Min(a, b); i++)
+            {
+                if (Math.Max(a,b) % divizor == 0 && Math.Min(a,b) % divizor == 0)
+                {
+                    break;
+                }
+                divizor--;
+            }
+
+            return divizor;
+        }
+    }
+}
